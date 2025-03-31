aa0a94dc4239519fe7046e10841d7adf9b5c2a9c
mayunhao <mayunhao@tj-un.com>
2025-03-31 15:50:01 +0800

    init

---
+++ auth.ts
@@ -0,0 +1,76 @@
+import NextAuth from "next-auth"
+import "next-auth/jwt"
+
+import GitHub from "next-auth/providers/github"
+import Google from "next-auth/providers/google"
+import { createStorage } from "unstorage"
+import memoryDriver from "unstorage/drivers/memory"
+import vercelKVDriver from "unstorage/drivers/vercel-kv"
+import { UnstorageAdapter } from "@auth/unstorage-adapter"
+import Gitee from '@/providers/gitee';
+
+
+const storage = createStorage({
+  driver: process.env.VERCEL
+    ? vercelKVDriver({
+      url: process.env.AUTH_KV_REST_API_URL,
+      token: process.env.AUTH_KV_REST_API_TOKEN,
+      env: false,
+    })
+    : memoryDriver(),
+})
+
+export const { handlers, auth, signIn, signOut } = NextAuth({
+  debug: true,
+  theme: { logo: "https://authjs.dev/img/logo-sm.png" },
+  adapter: UnstorageAdapter(storage),
+  providers: [
+    GitHub({
+      clientId: process.env.AUTH_GITHUB_ID!,
+      clientSecret: process.env.AUTH_GITHUB_SECRET!,
+    }),
+    Google({
+      clientId: process.env.AUTH_GOOGLE_ID!,
+      clientSecret: process.env.AUTH_GOOGLE_SECRET!,
+    }),
+    Gitee({
+      clientId: process.env.GITEE_CLIENT_ID,
+      clientSecret: process.env.GITEE_CLIENT_SECRET,
+    }),
+  ],
+  basePath: "/auth",
+  session: { strategy: "jwt" },
+  secret: "yQsP/7mO+01kY/pLXiTSx0czRY1RNUw7u97KVclTvwM=",
+  callbacks: {
+    authorized({ request, auth }) {
+      const { pathname } = request.nextUrl
+      if (pathname === "/middleware-example") return !!auth
+      return true
+    },
+    jwt({ token, trigger, session, account }) {
+      if (trigger === "update") token.name = session.user.name
+      if (account?.provider === "keycloak") {
+        return { ...token, accessToken: account.access_token }
+      }
+      return token
+    },
+    async session({ session, token }) {
+      if (token?.accessToken) session.accessToken = token.accessToken
+
+      return session
+    },
+  },
+  experimental: { enableWebAuthn: true },
+})
+
+declare module "next-auth" {
+  interface Session {
+    accessToken?: string
+  }
+}
+
+declare module "next-auth/jwt" {
+  interface JWT {
+    accessToken?: string
+  }
+}
